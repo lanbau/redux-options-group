f9508ba554c1e88a11737d43306d87ae6aa804c0
irvin lin <irvinln@gmail.com>
2019-06-28 17:48:45 +0800

    basic setup

--- src/index.js
+++ src/index.js
@@ -1,10 +1,33 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
+import { createStore, applyMiddleware } from "redux";
+import "./index.css";
+import App from "./App";
+import * as serviceWorker from "./serviceWorker";
+import { reducer as formReducer } from "redux-form";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import todos from "./reducers/todos";
+import { combineReducers } from "redux";
+
+import logger from "redux-logger";
+
+const rootReducer = combineReducers({
+  // ...your other reducers here
+  // you have to pass formReducer under 'form' key,
+  // for custom keys look up the docs for 'getFormState'
+  todos,
+  form: formReducer
+});
+
+const store = createStore(rootReducer, applyMiddleware(logger));
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById("root")
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
